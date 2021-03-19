57f15f8ce8a46f4b80719b7bd6f5cc17f6f08fe2
FelHab09 <felhab09@gmail.com>
2021-03-18 20:30:47 -0400

    Partie 2 terminee
    Labo4

--- main_cm4.c
+++ main_cm4.c
@@ -25,10 +25,38 @@ void ChangeLed(){
     Cy_GPIO_Write(GreenLed_0_PORT,GreenLed_0_NUM,1);
     }
 }
+SemaphoreHandle_t Semaphore;
+int i=0;
+void isr_bouton(){
+    i++;
+    xSemaphoreGiveFromISR(Semaphore,NULL);
+    Cy_GPIO_ClearInterrupt(Bouton_0_PORT,Bouton_0_NUM);
+    NVIC_ClearPendingIRQ(Bouton_ISR_cfg.intrSrc);
+}
+void bouton_task(){
+    for(;;){
+        xSemaphoreTake(Semaphore,portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(20));
+        if (i%2!=0){
+        UART_PutString("Bouton appuye \r\n");
+        }
+        else{
+        UART_PutString("Bouton relache \r\n");
+        }
+        xSemaphoreGive(Semaphore);
+    }
+}
+
 int main(void)
 {
+    Semaphore=xSemaphoreCreateBinary();
     __enable_irq();
+    UART_Start();
+    Cy_SysInt_Init(&Bouton_ISR_cfg, isr_bouton);
+    NVIC_ClearPendingIRQ(Bouton_ISR_cfg.intrSrc);
+    NVIC_EnableIRQ(Bouton_ISR_cfg.intrSrc);
     xTaskCreate(ChangeLed,"Change Led",80,NULL,3,NULL);
+    xTaskCreate(bouton_task,"Bouton task",80,NULL,3,NULL);
     vTaskStartScheduler();
     for(;;)
     {
